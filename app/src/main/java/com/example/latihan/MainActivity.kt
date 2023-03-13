57bb8560f147c8fd4a05bd5d142fb6aa74dd5120
FaridRifaan <faridrifan59@gmail.com>
2023-03-13 22:28:29 +0700

    latihan linear layout

--- app/src/main/java/com/example/latihan/MainActivity.kt
+++ app/src/main/java/com/example/latihan/MainActivity.kt
@@ -7,11 +7,22 @@ import com.example.latihan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+//    val camelCase1 = 26
+
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 //        setContentView(R.layout.activity_main)
     }
+//    fun camelCase(){
+//        println(Companion.USER_ID)
+//        println(camelCase1)
+//    }
+//
+//    companion object {
+//        const val USER_ID = 2
+//    }
+
 
 }
